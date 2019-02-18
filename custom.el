31354072564c758bc0fcf6e8c87d22e0ddf67bc9
dakodeon <dakodeon@hotmail.com>
2019-02-19 00:33:46 +0200

    Revert "got rid of custom.el"
    This reverts commit 4ada386522b682b4e1a7152d2fc32147ec009cad.

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("5f4e4c9f5de8156f964fdf8a1b8f8f659efbfeff88b38f49ce13953a84272b77" default)))
+ '(default-input-method "greek")
+ '(helm-source-names-using-follow (quote ("Occur")))
+ '(package-selected-packages
+   (quote
+    (mu4e-alert helm try swoop yasnippet-snippets yasnippet auto-complete latex-preview-pane use-package pdf-tools auctex auctex-latexmk expand-region ido-vertical-mode
+		(\, ace-window)
+		rainbow-delimiters smartparens klere-theme darkokai-theme))))
+ 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Liberation Mono" :foundry "nil" :width normal :height 100 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#DCDCCC" :background "#3F3F3F" :stipple nil :inherit nil))))
+ '(helm-selection ((t (:underline nil :background "MediumOrchid1" :foreground "white")))))

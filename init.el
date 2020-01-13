4e6c4e6df50fd2fbd12c71ed077fafe2311cf4d1
dakodeon <dakodeon@hotmail.com>
2020-01-13 21:56:53 +0200

    Added auto update

--- init.el
+++ init.el
@@ -13,10 +13,12 @@
 
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "http://melpa.org/packages/")
+			 ;; ("melpa" . "http://melpa.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")
 			 ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
+(package-refresh-contents)
 
 ;; Bootstrap use-package
 (unless (package-installed-p 'use-package)

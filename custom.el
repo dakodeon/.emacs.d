8631eba34cc9d1421216286e579f58b9e284aaec
dakodeon <dakodeon@hotmail.com>
2020-04-08 04:40:10 +0300

    First one in a long time, see details
    Replaced helm with ivy, made some mu4e tweaks, added general package.
    Some functions here and there (peep-mark-backwards) and some defaults

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(default-input-method "greek")
  '(package-selected-packages
    (quote
-    (font-lock+ dired-atool emojify 2048-game evil-collection org-msg org-plus-contrib pdf-tools all-the-icons-dired dired-rainbow dired-rsync dired-ranger dired-narrow dired-subtree ace-mc mc-extras multiple-cursors auto-complete counsel helm-swoop helm ace-window restart-emacs expand-region rainbow-delimiters smartparens which-key delight all-the-icons color-theme-sanityinc-tomorrow rainbow-mode darkroom linum-relative try use-package))))
+    (general amx ivy-posframe ivy-rich font-lock+ dired-atool emojify 2048-game evil-collection org-msg org-plus-contrib pdf-tools all-the-icons-dired dired-rainbow dired-rsync dired-ranger dired-narrow dired-subtree ace-mc mc-extras multiple-cursors auto-complete counsel helm-swoop helm ace-window restart-emacs expand-region rainbow-delimiters smartparens which-key delight all-the-icons color-theme-sanityinc-tomorrow rainbow-mode darkroom linum-relative try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

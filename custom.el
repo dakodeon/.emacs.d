a4bb4a217003e48cc526511013267e955764a2f5
dakodeon <dakodeon@hotmail.com>
2020-05-03 15:27:21 +0300

    Rebetika function, some evil-binds for org

--- custom.el
+++ custom.el
@@ -17,7 +17,7 @@
  '(org-contacts-files (quote ("~/.personal/contacts.org")) t)
  '(package-selected-packages
    (quote
-    (gotham-theme darktooth-theme modus-vivendi-theme mc-extrasaaaa window multi-vterm auctex metronome expand-region patates general amx ivy-posframe ivy-rich font-lock+ dired-atool emojify 2048-game evil-collection org-msg org-plus-contrib pdf-tools all-the-icons-dired dired-rainbow dired-rsync dired-ranger dired-narrow dired-subtree auto-complete counsel helm-swoop helm ace-window restart-emacs rainbow-delimiters smartparens which-key delight all-the-icons rainbow-mode darkroom linum-relative try use-package))))
+    (fzf gotham-theme darktooth-theme modus-vivendi-theme mc-extrasaaaa window multi-vterm auctex metronome expand-region patates general amx ivy-posframe ivy-rich font-lock+ dired-atool emojify 2048-game evil-collection org-msg org-plus-contrib pdf-tools all-the-icons-dired dired-rainbow dired-rsync dired-ranger dired-narrow dired-subtree auto-complete counsel helm-swoop helm ace-window restart-emacs rainbow-delimiters smartparens which-key delight all-the-icons rainbow-mode darkroom linum-relative try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

06e14eac1660d80e1376aa5f563257ccc32b42f8
dakodeon <dakodeon@hotmail.com>
2020-11-30 10:51:48 +0200

    huge commit, vimified, themes, etc...

--- custom.el
+++ custom.el
@@ -3,27 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(amx-backend (quote auto))
- '(amx-save-file "~/.emacs.d/amx-items")
- '(custom-safe-themes
-   (quote
-    ("79485bab8bb220562d4acd003e4b6f1c9005af41e91f81b7a0e89b7e3a301203" default)))
- '(default-input-method "greek")
- '(ivy-posframe-display-functions-alist
-   (quote
-    ((complete-symbol . ivy-posframe-display-at-point)
-     (counsel-describe-function)
-     (counsel-describe-variable)
-     (swiper)
-     (swiper-isearch)
-     (t . ivy-posframe-display-at-frame-center))))
- '(org-contacts-files (quote ("~/.personal/contacts.org")) t)
  '(package-selected-packages
-   (quote
-    (subed fzf gotham-theme darktooth-theme modus-vivendi-theme mc-extrasaaaa window multi-vterm auctex metronome expand-region patates general amx ivy-posframe ivy-rich font-lock+ dired-atool emojify 2048-game evil-collection org-msg org-plus-contrib pdf-tools all-the-icons-dired dired-rainbow dired-rsync dired-ranger dired-narrow dired-subtree auto-complete counsel helm-swoop helm ace-window restart-emacs rainbow-delimiters smartparens which-key delight all-the-icons rainbow-mode darkroom linum-relative try use-package))))
+   '(which-key web-mode use-package undo-tree try smartparens restart-emacs rainbow-mode rainbow-delimiters projectile org-plus-contrib org-msg openwith multi-vterm metronome mc-extras lorem-ipsum linum-relative ivy-rich ivy-posframe helm-swoop general fzf font-lock+ expand-region evil-surround evil-commentary evil-collection emojify doom-themes doom-modeline dired-subtree dired-rsync dired-ranger dired-rainbow dired-narrow dired-atool delight darkroom counsel company-quickhelp autothemer auto-complete auctex amx all-the-icons-dired ace-window ace-mc 2048-game)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ivy-posframe ((t (:background "#121212"))))
+ '(mode-line ((t (:background "#00688B"))))
+ '(org-block ((t (:extend nil))))
+ '(org-block-begin-line ((t (:extend nil :background "#121212" :foreground "#579C4C"))))
+ '(org-block-end-line ((t (:extend nil)))))
